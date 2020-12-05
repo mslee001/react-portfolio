6f43be963649269e8ae8c14523fcbafb8419149e
mslee001 <megtej@gmail.com>
2020-12-04 23:43:53 -0800

    updated path to header and footer components

--- src/components/Pages/Projects.js
+++ src/components/Pages/Projects.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Project from "./../Project";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "../Header";
+import Footer from "/../Footer";
 
 function Projects() {
     return(
