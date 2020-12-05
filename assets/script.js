9034126affc375e6b422edf87c58d54519358fcd
mslee001 <megtej@gmail.com>
2020-12-04 21:46:58 -0800

    Updates

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    
+    $('#header-btn').on('click', function(e) {
+    $('#header-menu').toggleClass('active');
+    $('.nav-btn').toggleClass('active');
+});
+
+// Hide menu after clicking menu item
+$('.dropdown-menu li').on('click', function(e) {
+     $('#header-menu').removeClass('active');
+     $('.nav-btn').removeClass('active');
+});
+});
