a55c198a7a77954ff6bd02533422cbbcb58edddc
mslee001 <megtej@gmail.com>
2020-12-04 21:36:22 -0800

    Landing page which allows the user to enter site

---
+++ src/components/Pages/Home.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Home() {
+
+    return (
+        <div>
+        <div id="splash-img" className="hero fullscreen hero-img parallax-img">
+        <div className="hero-body">
+            <div className="content u-text-center">
+                <div>
+                    <h1 className="uppercase white title">Welcome to Megan's Portfolio</h1>
+                    <h3 className="uppercase white sub-title faded">Please enter</h3>
+                </div>
+                <a href="/About" className="u-inline-block">
+                    <button className="btn-light outline">
+                        About Me
+                    </button>
+                </a>
+                <a href="/Projects" className="u-inline-block">
+                    <button className="btn-light outline">
+                        Projects
+                    </button>
+                </a>
+            </div>
+        </div>
+    </div>
+        </div>
+    );
+}
+
+export default Home;
