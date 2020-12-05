e824796e754cbdefcc5cda494d045c9ed0fc6cb1
mslee001 <megtej@gmail.com>
2020-12-04 23:46:06 -0800

    fixed footer path

--- src/components/Pages/Projects.js
+++ src/components/Pages/Projects.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Project from "./../Project";
 import Header from "../Header";
-import Footer from "/../Footer";
+import Footer from "../Footer";
 
 function Projects() {
     return(
